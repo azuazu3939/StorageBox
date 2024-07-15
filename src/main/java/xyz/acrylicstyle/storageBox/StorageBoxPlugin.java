50322d854a5b2ec443327e18a33f66dae3fe785f
あず <98195581+azuazu3939@users.noreply.github.com>
2024-07-15 15:31:42 +0900

    mcMMO対応

--- src/main/java/xyz/acrylicstyle/storageBox/StorageBoxPlugin.java
+++ src/main/java/xyz/acrylicstyle/storageBox/StorageBoxPlugin.java
@@ -1,51 +1,46 @@
 package xyz.acrylicstyle.storageBox;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.GameMode;
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
-import org.bukkit.Sound;
+import com.gmail.nossr50.api.ItemSpawnReason;
+import com.gmail.nossr50.datatypes.meta.BonusDropMeta;
+import com.gmail.nossr50.datatypes.skills.PrimarySkillType;
+import com.gmail.nossr50.mcMMO;
+import org.bukkit.*;
 import org.bukkit.block.BlockState;
-import org.bukkit.block.Chest;
-import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDispenseEvent;
+import org.bukkit.event.block.BlockDropItemEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 import xyz.acrylicstyle.storageBox.utils.StorageBox;
 import xyz.acrylicstyle.storageBox.utils.StorageBoxUtils;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.UUID;
+import java.util.*;
 import java.util.logging.Logger;
 
 public class StorageBoxPlugin extends JavaPlugin implements Listener {
     public static Logger LOGGER;
     public static List<UUID> bypassingPlayers = new ArrayList<>();
-    public static int customModelData = 200000000;
+    public static Integer customModelData = null;
 
     @Override
     public void onEnable() {
         LOGGER = getLogger();
         saveDefaultConfig();
-        customModelData = getConfig().getInt("custom-model-data", customModelData);
+        customModelData = (Integer) getConfig().get("custom-model-data");
         Objects.requireNonNull(Bukkit.getPluginCommand("storagebox")).setTabCompleter(new StorageBoxTabCompleter());
         Objects.requireNonNull(Bukkit.getPluginCommand("storagebox")).setExecutor(new RootCommand());
         Bukkit.getPluginManager().registerEvents(this, this);
@@ -60,6 +55,7 @@ public class StorageBoxPlugin extends JavaPlugin implements Listener {
         }
     }
 
+    /*
     @Override
     public void onDisable() {
         LOGGER.info("Saving config");
@@ -70,49 +66,53 @@ public class StorageBoxPlugin extends JavaPlugin implements Listener {
         }
         LOGGER.info("Saved config");
     }
+    */
 
     public void run(Runnable runnable) { Bukkit.getScheduler().runTask(this, runnable); }
 
     public void runAsync(Runnable runnable) { Bukkit.getScheduler().runTaskAsynchronously(this, runnable); }
 
-    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+    private boolean processing = false;
+    @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
     public void onBlockPlace(BlockPlaceEvent e) {
-        ItemStack item = e.getPlayer().getInventory().getItemInMainHand();
-        boolean mainHand = true;
-        StorageBox storageBox = StorageBox.getStorageBox(item);
+        if (processing) return;
+        boolean mainHand = e.getHand() == EquipmentSlot.HAND;
+        StorageBox storageBox = StorageBox.getStorageBox(mainHand ? e.getPlayer().getInventory().getItemInMainHand() : e.getPlayer().getInventory().getItemInOffHand());
         if (storageBox == null) {
-            item = e.getPlayer().getInventory().getItemInOffHand();
-            storageBox = StorageBox.getStorageBox(item);
-            if (storageBox == null) return;
-            mainHand = false;
+            return;
         }
         if (storageBox.isEmpty()) {
             e.getPlayer().sendMessage(ChatColor.RED + "Storage Boxが空です。");
             e.setCancelled(true);
             return;
         }
-        boolean finalMainHand1 = mainHand;
-        StorageBox finalStorageBox = storageBox;
-        runAsync(() -> {
-            finalStorageBox.decreaseAmount();
-            run(() -> {
-                if (finalMainHand1) {
-                    e.getPlayer().getInventory().setItemInMainHand(finalStorageBox.getItemStack());
-                } else {
-                    e.getPlayer().getInventory().setItemInOffHand(finalStorageBox.getItemStack());
-                }
-                BlockState state = e.getBlockPlaced().getState();
-                if (state instanceof Chest) {
-                    ((Chest) state).setCustomName(null);
-                }
-            });
+        BlockState placedState = e.getBlockPlaced().getState();
+        e.setCancelled(true);
+        storageBox.decreaseAmount();
+        if (mainHand) {
+            e.getPlayer().getInventory().setItemInMainHand(storageBox.getItemStack());
+        } else {
+            e.getPlayer().getInventory().setItemInOffHand(storageBox.getItemStack());
+        }
+        run(() -> {
+            BlockPlaceEvent event = new BlockPlaceEvent(e.getBlockPlaced(), e.getBlockReplacedState(), e.getBlockAgainst(), e.getItemInHand(), e.getPlayer(), e.canBuild(), e.getHand());
+            processing = true;
+            try {
+                Bukkit.getPluginManager().callEvent(event);
+            } finally {
+                processing = false;
+            }
+            if (!event.isCancelled()) {
+                placedState.update(true, true);
+            }
         });
     }
 
     @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
-    public void onPlayerAttemptPickupItem(EntityPickupItemEvent e) {
-        if (!(e.getEntity() instanceof Player player)) return;
-        if (!new ItemStack(e.getItem().getItemStack().getType()).isSimilar(e.getItem().getItemStack())) return;
+    public void onPlayerAttemptPickupItem(@NotNull EntityPickupItemEvent e) {
+        if (!(e.getEntity() instanceof Player)) return;
+        Player player = (Player) e.getEntity();
+        if (e.getItem().getItemStack().hasItemMeta()) return;
         if (StorageBox.getStorageBox(e.getItem().getItemStack()) != null) return;
         Map.Entry<Integer, StorageBox> storageBox = StorageBoxUtils.getStorageBoxForType(player.getInventory(), e.getItem().getItemStack());
         if (storageBox == null) return;
@@ -125,17 +125,86 @@ public class StorageBoxPlugin extends JavaPlugin implements Listener {
         player.getInventory().setItem(storageBox.getKey(), storageBox.getValue().getItemStack());
     }
 
+    @EventHandler
+    public void onBlockDropItemMcMMO(@NotNull BlockDropItemEvent e) {
+        Set<Material> uniqueMaterials = new HashSet<>();
+        boolean doNotRewardTE = false;
+        int blockCount = 0;
+
+        for (Item item : e.getItems()) {
+            Material m = item.getItemStack().getType();
+            uniqueMaterials.add(m);
+            if (m.isBlock()) {
+                blockCount++;
+            }
+        }
+        if (uniqueMaterials.size() > 1) {
+            doNotRewardTE = true;
+        }
+        int bonus = 0;
+        if (blockCount <= 1) {
+            for (Item item : e.getItems()) {
+                ItemStack is = new ItemStack(item.getItemStack());
+
+                if (is.getAmount() <= 0) continue;
+                if (!mcMMO.p.getGeneralConfig().getDoubleDropsEnabled(PrimarySkillType.MINING, is.getType())
+                        && !mcMMO.p.getGeneralConfig().getDoubleDropsEnabled(PrimarySkillType.HERBALISM, is.getType())
+                        && !mcMMO.p.getGeneralConfig().getDoubleDropsEnabled(PrimarySkillType.WOODCUTTING, is.getType()))
+                    continue;
+
+                if (doNotRewardTE) {
+                    if (!is.getType().isBlock()) {
+                        continue;
+                    }
+                }
+
+                if (!e.getBlock().getMetadata("mcMMO: Double Drops").isEmpty()) {
+                    BonusDropMeta bonusDropMeta = (BonusDropMeta) e.getBlock().getMetadata("mcMMO: Double Drops").get(0);
+                    int bonusCount = bonusDropMeta.asInt();
+
+                    for (int i = 0; i < bonusCount; i++) {
+                        if (is.getType() == Material.AIR || e.getBlockState().getLocation() == null) continue;
+                        bonus++;
+                    }
+                }
+            }
+        }
+        collect(e, e.getPlayer(), bonus);
+    }
+
+    private void collect(@NotNull BlockDropItemEvent e, Player p, int bonus) {
+        boolean check = false;
+        List<Item> toRemove = new ArrayList<>();
+        for (Item item : e.getItems()) {
+            if (item.getItemStack().hasItemMeta()) continue;
+            Map.Entry<Integer, StorageBox> storageBox = StorageBoxUtils.getStorageBoxForType(p.getInventory(), item.getItemStack());
+            if (storageBox == null) return;
+            e.setCancelled(true);
+            check = true;
+            long amount = item.getItemStack().getAmount();
+            item.remove();
+//            player.playSound(player.getLocation(), Sound.ENTITY_ITEM_PICKUP, 0.8F, 1.9F);
+            storageBox.getValue().setAmount(storageBox.getValue().getAmount() + amount + bonus);
+            p.getInventory().setItem(storageBox.getKey(), storageBox.getValue().getItemStack());
+            toRemove.add(item);
+        }
+        e.getItems().removeAll(toRemove);
+        if (e.getBlock().hasMetadata("mcMMO: Double Drops") && check)
+            e.getBlock().removeMetadata("mcMMO: Double Drops", getPlugin(mcMMO.class));
+    }
+
     @EventHandler(priority = EventPriority.LOWEST)
-    public void onBlockDispense(BlockDispenseEvent e) {
+    public void onBlockDispense(@NotNull BlockDispenseEvent e) {
         if (StorageBox.getStorageBox(e.getItem()) != null) e.setCancelled(true);
     }
 
-    private static ItemStack n(ItemStack item) {
+    @Contract("null -> new; !null -> param1")
+    private static @NotNull ItemStack n(ItemStack item) {
         return item == null ? new ItemStack(Material.AIR) : item;
     }
 
     @EventHandler
-    public void onPrepareItemCraft(PrepareItemCraftEvent e) {
+    public void onPrepareItemCraft(@NotNull PrepareItemCraftEvent e) {
         ItemStack[] matrix = e.getInventory().getMatrix();
         if (matrix.length == 9) {
             if (
@@ -165,7 +234,7 @@ public class StorageBoxPlugin extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void onInventoryClick(InventoryClickEvent e) {
+    public void onInventoryClick(@NotNull InventoryClickEvent e) {
         if (e.getWhoClicked().getGameMode() == GameMode.CREATIVE) return;
         if (e.getClickedInventory() == null) return;
         if (e.getCurrentItem() == null || e.getCurrentItem().getType().isAir()) return;
@@ -192,10 +261,34 @@ public class StorageBoxPlugin extends JavaPlugin implements Listener {
         }
     }*/
 
-    public static int getEmptySlots(Player p) {
+    public static int getEmptySlots(@NotNull Player p) {
         ItemStack[] cont = p.getInventory().getContents();
         int i = 0;
         for (ItemStack item : cont) if (item == null || item.getType() == Material.AIR) i++;
+        if (p.getInventory().getHelmet() == null || p.getInventory().getHelmet().getType() == Material.AIR) i--;
+        if (p.getInventory().getChestplate() == null || p.getInventory().getChestplate().getType() == Material.AIR) i--;
+        if (p.getInventory().getLeggings() == null || p.getInventory().getLeggings().getType() == Material.AIR) i--;
+        if (p.getInventory().getBoots() == null || p.getInventory().getBoots().getType() == Material.AIR) i--;
+        if (p.getInventory().getItemInOffHand().getType() == Material.AIR) i--;
         return i;
+    }
+
+    public static @NotNull StorageBoxPlugin getInstance() {
+        return getPlugin(StorageBoxPlugin.class);
+    }
+
+    @Contract(pure = true)
+    private @NotNull List<ItemSpawnReason> getReasons() {
+        return Arrays.asList(
+                ItemSpawnReason.BONUS_DROPS,
+                ItemSpawnReason.TREE_FELLER_DISPLACED_BLOCK,
+                ItemSpawnReason.EXCAVATION_TREASURE);
+    }
+
+    private boolean shouldCollectReasons(ItemSpawnReason reason) {
+        for (ItemSpawnReason r : getReasons()) {
+            if (r.equals(reason)) return true;
+        }
+        return false;
     }
 }
